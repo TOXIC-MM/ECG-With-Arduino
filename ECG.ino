75e47bfb46819a966bf02a2a55fe60f4d1a3c5db
Mohammad Mahdi Hajimobini <83955863+TOXIC-MM@users.noreply.github.com>
2024-02-01 02:13:30 +0330

    Update ECG.ino

--- ECG.ino
+++ ECG.ino
@@ -2,13 +2,14 @@ void setup() {
 Serial.begin(9600);
 pinMode(8, INPUT); // Setup for leads off detection LO +
 pinMode(9, INPUT); // Setup for leads off detection LO -
+//Connect Output Pin To A0
 }
 void loop() {
-if((digitalRead(8) == 1) || (digitalRead(9) == 1)){ //بررسی وجود الکترود ها
+if((digitalRead(8) == 1) || (digitalRead(9) == 1)){ //Checking If Electrodeds Are Connectec?
 Serial.println("leads off!");
 }
 else{
-Serial.println(analogRead(A0));
+Serial.println(analogRead(A0)); // Reading Data From A0
 }
 delay(1);
 }
